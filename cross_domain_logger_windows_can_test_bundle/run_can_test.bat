6fd94c7ce931dd94170cfb0e433332e22af0808c
Bharathh <bharath.busireddy@gmail.com>
2026-02-28 01:06:11 -0500

    v2

--- cross_domain_logger_windows_can_test_bundle/run_can_test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-echo Running CAN test...
-cross_domain_logger_windows.exe --test-can
-echo.
-echo Output saved only in console. Press any key to close.
-pause >nul
